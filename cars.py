ece3714592ddf036ae710a455143fd5e5f7fea9c
MaxLee77 <100952237+MaxLee77@users.noreply.github.com>
2022-03-04 21:15:15 +0000

    new file cars

---
+++ cars.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 15:51:57 2022
+
+@author: msii
+"""
+
+cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+for car in cars:
+    if car != 'gm':
+        print(car.title())
+    else:
+      print(car.upper())
+
+        
